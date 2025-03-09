110420c504e825c8ac70cb46f503404189be58cf
Sanjaykumar Rathva <sanjayrathva091@gmail.com>
2025-03-09 06:04:02 +0000

    test: 2. Translate I ate yogurt for breakfast. to British English

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,7 +5,7 @@ const Translator = require('../components/translator.js');
 
 suite('Unit Tests', () => {
   const translator = new Translator();
-  
+
   // Test 1: Translate "Mangoes are my favorite fruit." to British English
   test('Translate "Mangoes are my favorite fruit." to British English', () => {
     const text = 'Mangoes are my favorite fruit.';
@@ -13,5 +13,14 @@ suite('Unit Tests', () => {
     const result = translator.translate(text, locale);
     assert.include(result, 'favourite');
     assert.include(result, '<span class="highlight">favourite</span>');
+  });
+
+  // Test 2: Translate "I ate yogurt for breakfast." to British English
+  test('Translate "I ate yogurt for breakfast." to British English', () => {
+    const text = 'I ate yogurt for breakfast.';
+    const locale = 'american-to-british';
+    const result = translator.translate(text, locale);
+    assert.include(result, 'yoghurt');
+    assert.include(result, '<span class="highlight">yoghurt</span>');
   });
 });

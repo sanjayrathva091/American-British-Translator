07ffe6a456b7eb58a694ad5f077f93b5fc246b20
Sanjaykumar Rathva <sanjayrathva091@gmail.com>
2025-03-09 06:02:31 +0000

    test: 1. Translate Mangoes are my favorite fruit. to British English

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,5 +4,14 @@ const assert = chai.assert;
 const Translator = require('../components/translator.js');
 
 suite('Unit Tests', () => {
-
+  const translator = new Translator();
+  
+  // Test 1: Translate "Mangoes are my favorite fruit." to British English
+  test('Translate "Mangoes are my favorite fruit." to British English', () => {
+    const text = 'Mangoes are my favorite fruit.';
+    const locale = 'american-to-british';
+    const result = translator.translate(text, locale);
+    assert.include(result, 'favourite');
+    assert.include(result, '<span class="highlight">favourite</span>');
+  });
 });
